9813eed1fb64107b532aa6e9f10cf9b7ecce75e0
SheharaF01 <shehara.oddly@gmail.com>
2026-02-07 22:12:18 +0530

    Create UserPermissionsAPITestRunner.java

---
+++ src/test/java/runners/UserPermissionsAPITestRunner.java
@@ -0,0 +1,13 @@
+package runners;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features/api/user_permissions_api.feature",
+        glue = {"stepdefinitions.api"},
+        plugin = {"pretty"}
+)
+public class UserPermissionsAPITestRunner {}
